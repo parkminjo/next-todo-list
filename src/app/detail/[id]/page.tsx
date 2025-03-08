f0a267a9f0116042effbfe16448161a0cf1ab8df
Minjo Park <consistent.312@gmail.com>
2025-03-08 10:27:49 +0900

    add: 할 일 디테일 페이지 구현

--- src/app/detail/[id]/page.tsx
+++ src/app/detail/[id]/page.tsx
@@ -1,3 +1,34 @@
-const TodoDetail = () => {};
+"use client";
+
+import { fetchDetailData } from "@/api/todos";
+import { Todos } from "@/components/types";
+import { Typography } from "@/components/ui/Typography";
+import { useParams } from "next/navigation";
+import { useEffect, useState } from "react";
+
+const TodoDetail = () => {
+  const params = useParams();
+  const todoId = typeof params.id === "string" ? params.id : undefined;
+
+  const [todo, setTodo] = useState<Todos | null>();
+
+  useEffect(() => {
+    if (!todoId) return;
+
+    const fetchDetailTodo = async (todoId: string) => {
+      const data = await fetchDetailData(todoId);
+      console.log(data);
+      setTodo(data);
+    };
+
+    fetchDetailTodo(todoId);
+  }, [todoId]);
+
+  return (
+    <div>
+      <Typography>{todo?.title}</Typography>
+    </div>
+  );
+};
 
 export default TodoDetail;
