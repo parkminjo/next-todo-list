1a22b48ac2c37c6a828b0ad1ae01ec6457b55896
parkminjo <cruiser367@naver.com>
2025-06-10 20:15:22 +0900

    refactor: todoId 타입 수정

--- src/features/todo/api/deleteTodo.ts
+++ src/features/todo/api/deleteTodo.ts
@@ -1,7 +1,8 @@
 import { ENV } from '@/shared/constants/env';
 import { METHOD } from '@/shared/constants/http-method';
+import type { Todo } from '@/features/todo/types/todo.type';
 
-export const deleteTodo = async (todoId: string) => {
+export const deleteTodo = async (todoId: Todo['id']) => {
   console.log(todoId);
   await fetch(`${ENV.JSON_SERVER_URL}/${todoId}`, {
     method: METHOD.DELETE,
