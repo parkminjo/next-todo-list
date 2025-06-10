6c107016bb1190a196810df298e9c7c809268300
parkminjo <cruiser367@naver.com>
2025-06-10 21:28:54 +0900

    refactor: useTodoListQuery 분리

---
+++ src/features/todo/hooks/useTodoListQuey.ts
@@ -0,0 +1,9 @@
+import { useQuery } from '@tanstack/react-query';
+import { getTodoList } from '@/features/todo/api/client/getTodoList';
+
+export const useTodoListQuery = () => {
+  return useQuery({
+    queryKey: ['todoList'],
+    queryFn: getTodoList,
+  });
+};
