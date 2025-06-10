0ec0d87287085dbf24c0436e26ed622d8cc98d17
parkminjo <cruiser367@naver.com>
2025-06-10 19:53:51 +0900

    fix: 클라이언트 컴포넌트에서 fetch 사용하면 발생하는 오류 해결

--- src/shared/constants/env.ts
+++ src/shared/constants/env.ts
@@ -1,12 +1,3 @@
 export const ENV = {
-  JSON_SERVER_URL: getEnvVariable('JSON_SERVER_URL'),
+  JSON_SERVER_URL: process.env.NEXT_PUBLIC_JSON_SERVER_URL ?? '',
 };
-
-function getEnvVariable(key: string) {
-  const value = process.env[key];
-
-  if (!value) {
-    throw new Error(`환경 변수 ${key}가 누락되었습니다.`);
-  }
-  return value;
-}
