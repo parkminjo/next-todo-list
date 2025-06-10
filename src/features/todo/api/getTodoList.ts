8d71614b05bd2e9f262bfa9f8eaa55b90895b59d
parkminjo <cruiser367@naver.com>
2025-06-10 21:08:57 +0900

    refactor: response.ok가 아닐 때 에러 반환하는 로직 추가

--- src/features/todo/api/getTodoList.ts
+++ src/features/todo/api/getTodoList.ts
@@ -2,6 +2,10 @@ import { ENV } from '@/shared/constants/env';
 
 export const getTodoList = async () => {
   const response = await fetch(ENV.JSON_SERVER_URL);
+  if (!response.ok) {
+    throw new Error('todoList를 불러오는데 실패하였습니다.');
+  }
+
   const todoList = await response.json();
 
   return todoList;
