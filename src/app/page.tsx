a92555417368657825cec0a437c3a5899954dae9
parkminjo <cruiser367@naver.com>
2025-06-10 21:29:41 +0900

    refactor: 불필요한 코드 제거

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,11 @@
-import { getTodoList } from '@/features/todo/api/getTodoList';
 import TodoInput from '@/features/todo/components/todo-input';
 import TodoList from '@/features/todo/components/todo-list';
 
-const Home = async () => {
-  const todoList = await getTodoList();
-
+const Home = () => {
   return (
     <div className='flex flex-col gap-4'>
       <TodoInput />
-      <TodoList todoList={todoList} />
+      <TodoList />
     </div>
   );
 };
