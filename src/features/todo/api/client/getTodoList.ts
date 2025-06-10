b81ee40aff967dd4ca562fda011fdb21b138840c
parkminjo <cruiser367@naver.com>
2025-06-10 21:29:54 +0900

    refactor: 함수 반환값 타입 지정

--- src/features/todo/api/client/getTodoList.ts
+++ src/features/todo/api/client/getTodoList.ts
@@ -1,7 +1,7 @@
 import { ENV } from '@/shared/constants/env';
 import type { Todo } from '@/features/todo/types/todo.type';
 
-export const getTodoList = async (): Promise<Todo> => {
+export const getTodoList = async (): Promise<Todo[]> => {
   const response = await fetch(ENV.JSON_SERVER_URL);
   if (!response.ok) {
     throw new Error('todoList를 불러오는데 실패하였습니다.');
