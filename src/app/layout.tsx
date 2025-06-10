049c4b4f4a380502d6adc774c1b9770215bdb317
parkminjo <cruiser367@naver.com>
2025-06-10 22:10:42 +0900

    design: main에 flex-1 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({
         <TanstackQueryProviders>
           <LayoutWrapper>
             <Header />
-            <main className='px-4 py-2'>{children}</main>
+            <main className='flex-1 px-4 py-2'>{children}</main>
             <Footer />
           </LayoutWrapper>
         </TanstackQueryProviders>
