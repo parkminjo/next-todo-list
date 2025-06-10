44a6a8f5e26ffb5f371ce7221f538f442f01fdde
parkminjo <cruiser367@naver.com>
2025-06-10 22:30:03 +0900

    feat: not-found 페이지 추가

---
+++ src/app/not-found.tsx
@@ -0,0 +1,11 @@
+import LayoutWrapper from '@/shared/layout/layout-wrapper';
+
+const NotFound = () => {
+  return (
+    <LayoutWrapper>
+      <h1 className='text-2xl font-bold'>페이지를 찾을 수 없습니다.</h1>
+    </LayoutWrapper>
+  );
+};
+
+export default NotFound;
