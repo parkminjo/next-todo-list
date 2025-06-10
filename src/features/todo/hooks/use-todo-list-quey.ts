9fdc934eba2d8eec41664671329fdaf91f203ad6
parkminjo <cruiser367@naver.com>
2025-06-10 21:36:23 +0900

    refactor: querykey 상수로 분리

--- src/features/todo/hooks/use-todo-list-quey.ts
+++ src/features/todo/hooks/use-todo-list-quey.ts
@@ -1,9 +1,10 @@
 import { useQuery } from '@tanstack/react-query';
+import { QUERY_KEY } from '@/shared/constants/query-key';
 import { getTodoList } from '@/features/todo/api/client/get-todo-list';
 
 export const useTodoListQuery = () => {
   return useQuery({
-    queryKey: ['todoList'],
+    queryKey: [QUERY_KEY.TODO_LIST],
     queryFn: getTodoList,
   });
 };
