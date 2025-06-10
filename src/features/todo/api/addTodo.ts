06149c9699fd11f1df628f42ef96ed7a8b0186a6
parkminjo <cruiser367@naver.com>
2025-06-10 20:03:56 +0900

    refactor: method 상수로 분리

--- src/features/todo/api/addTodo.ts
+++ src/features/todo/api/addTodo.ts
@@ -1,9 +1,10 @@
 import { ENV } from '@/shared/constants/env';
 import type { Todo } from '@/features/todo/types/todo.type';
+import { METHOD } from '@/shared/constants/http-method';
 
 export const addTodo = async (todo: Omit<Todo, 'id'>) => {
   await fetch(ENV.JSON_SERVER_URL, {
-    method: 'POST',
+    method: METHOD.POST,
     headers: {
       'Content-Type': 'application/json',
     },
---
+++ src/shared/constants/http-method.ts
@@ -0,0 +1,7 @@
+export const METHOD = {
+  GET: 'GET',
+  POST: 'POST',
+  PATCH: 'PATCH',
+  PUT: 'PUT',
+  DELETE: 'DELETE',
+} as const;
