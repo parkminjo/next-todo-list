e0794bdf5f74f4369b6fc912e05445bd54b11bb5
parkminjo <cruiser367@naver.com>
2025-06-10 22:43:22 +0900

    feat: 재시도하는 기능 추가

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -2,14 +2,25 @@
 
 interface Props {
   error: Error;
+  reset: () => void;
 }
 
-const Error = ({ error }: Props) => {
+const Error = ({ error, reset }: Props) => {
+  const handleClick = () => {
+    reset();
+  };
+
   return (
     <div className='flex flex-col items-center gap-3'>
       <h1 className='text-2xl font-bold'>문제가 발생했습니다</h1>
       <p>죄송합니다. 요청을 처리하는 중에 오류가 발생했습니다.</p>
       <p className='text-sm text-gray-500'>오류 메시지: {error.message}</p>
+      <button
+        onClick={handleClick}
+        className='rounded-lg bg-gray-200 px-4 py-2 text-sm'
+      >
+        다시 시도하기
+      </button>
     </div>
   );
 };
