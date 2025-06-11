ba1b80c938ba03c023a3965bb120c4b6c9e367b7
parkminjo <cruiser367@naver.com>
2025-06-11 19:40:33 +0900

    refactor: stale-time 1분으로 지정

--- src/features/todo/hooks/use-todo-list-quey.ts
+++ src/features/todo/hooks/use-todo-list-quey.ts
@@ -1,5 +1,6 @@
 import { useQuery } from '@tanstack/react-query';
 import { QUERY_KEY } from '@/shared/constants/query-key';
+import { STALE_TIME } from '@/shared/constants/time';
 import { getTodoList } from '@/features/todo/api/get-todo-list';
 import { TODO_STATUS } from '@/features/todo/constants/todo-status';
 
@@ -7,5 +8,6 @@ export const useTodoListQuery = (todoStatus: keyof typeof TODO_STATUS) => {
   return useQuery({
     queryKey: [QUERY_KEY.TODO_LIST, todoStatus],
     queryFn: () => getTodoList(todoStatus),
+    staleTime: STALE_TIME.ONE_MINUTE_IN_MS,
   });
 };
