71a6c2f5e6acfb756e4bad879ec4901e852179a9
parkminjo <cruiser367@naver.com>
2025-06-11 17:43:18 +0900

    design: 최소 넓이 지정

--- src/shared/layout/layout-wrapper.tsx
+++ src/shared/layout/layout-wrapper.tsx
@@ -4,7 +4,7 @@ interface Props {
 
 const LayoutWrapper = ({ children }: Props) => {
   return (
-    <div className='mx-auto flex min-h-screen max-w-[400px] flex-col md:max-w-[800px]'>
+    <div className='mx-auto flex min-h-screen min-w-[320px] max-w-[400px] flex-col md:max-w-[800px]'>
       {children}
     </div>
   );
