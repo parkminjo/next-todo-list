b8d28c77f2a78f1c4e971268784b4d363e4f678e
parkminjo <cruiser367@naver.com>
2025-06-11 18:38:11 +0900

    design: 할일 완료 상태에 따라 필터해주는 select 구현

---
+++ src/features/todo/components/todo-status-select.tsx
@@ -0,0 +1,34 @@
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from '@/shared/ui/select';
+import React from 'react';
+import { TODO_STATUS } from '../constants/todo-status';
+
+interface Prop {
+  todoStatus: keyof typeof TODO_STATUS;
+  onChange: (value: keyof typeof TODO_STATUS) => void;
+}
+
+const TodoStatusSelect = ({ todoStatus, onChange }: Prop) => {
+  return (
+    <Select value={todoStatus} onValueChange={onChange}>
+      <SelectTrigger className='h-[30px] w-[100px]'>
+        <SelectValue placeholder='완료 상태' />
+      </SelectTrigger>
+      <SelectContent>
+        <SelectGroup>
+          <SelectItem value={TODO_STATUS.ALL}>전체</SelectItem>
+          <SelectItem value={TODO_STATUS.INCOMPLETE}>미완료</SelectItem>
+          <SelectItem value={TODO_STATUS.COMPLETED}>완료</SelectItem>
+        </SelectGroup>
+      </SelectContent>
+    </Select>
+  );
+};
+
+export default TodoStatusSelect;
