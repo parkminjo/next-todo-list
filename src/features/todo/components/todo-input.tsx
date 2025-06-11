d2ec077f8e8bffbdd0e87261a98760fcc158b95c
parkminjo <cruiser367@naver.com>
2025-06-11 17:20:02 +0900

    design: outline 색깔 변경

--- src/features/todo/components/todo-input.tsx
+++ src/features/todo/components/todo-input.tsx
@@ -39,7 +39,7 @@ const TodoInput = () => {
         placeholder='할 일을 추가해주세요'
         value={content}
         onChange={handleChangeInput}
-        className='w-full rounded-lg bg-gray-100 px-4 py-2'
+        className='w-full rounded-lg bg-gray-100 px-4 py-2 outline-gray-500'
       />
     </form>
   );
