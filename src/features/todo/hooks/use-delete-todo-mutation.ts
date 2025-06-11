eca8d98a7705eace9113c70b4f0b4bcfba6ecf4d
parkminjo <cruiser367@naver.com>
2025-06-11 22:58:34 +0900

    feat: 할 일 삭제 기능에 optimistic update 적용

--- src/features/todo/hooks/use-delete-todo-mutation.ts
+++ src/features/todo/hooks/use-delete-todo-mutation.ts
@@ -2,16 +2,35 @@ import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { toast } from 'react-toastify';
 import { deleteTodo } from '@/features/todo/api/delete-todo';
 import { QUERY_KEY } from '@/shared/constants/query-key';
+import type { Todo } from '@/features/todo/types/todo.type';
 
 export const useDeleteTodoMutation = () => {
   const queryClient = useQueryClient();
 
   return useMutation({
     mutationFn: deleteTodo,
-    onSuccess: () =>
-      queryClient.invalidateQueries({ queryKey: [QUERY_KEY.TODO_LIST] }),
-    onError: (error) => {
+    onMutate: (todoId: Todo['id']) => {
+      queryClient.cancelQueries({ queryKey: [QUERY_KEY.TODO_LIST] });
+      const previousTodoList = queryClient.getQueriesData<Todo[]>({
+        queryKey: [QUERY_KEY.TODO_LIST],
+      });
+
+      queryClient.setQueryData([QUERY_KEY.TODO_LIST], (old: Todo[] = []) => {
+        return old.filter((todo) => todo.id !== todoId);
+      });
+
+      return { previousTodoList };
+    },
+    onError: (error, variables, context) => {
+      if (context && context.previousTodoList) {
+        queryClient.setQueryData(
+          [QUERY_KEY.TODO_LIST],
+          context.previousTodoList,
+        );
+      }
       toast.error(error.message);
     },
+    onSettled: () =>
+      queryClient.invalidateQueries({ queryKey: [QUERY_KEY.TODO_LIST] }),
   });
 };
