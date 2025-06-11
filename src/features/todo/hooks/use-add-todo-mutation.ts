743564994898a45e8c0b188141935379abde36a0
parkminjo <cruiser367@naver.com>
2025-06-11 22:58:28 +0900

    feat: 할 일 추가 기능에 optimistic update 적용

--- src/features/todo/hooks/use-add-todo-mutation.ts
+++ src/features/todo/hooks/use-add-todo-mutation.ts
@@ -1,17 +1,36 @@
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { toast } from 'react-toastify';
-import { addTodo } from '@/features/todo/api/add-todo';
 import { QUERY_KEY } from '@/shared/constants/query-key';
+import { addTodo } from '@/features/todo/api/add-todo';
+import type { Todo } from '@/features/todo/types/todo.type';
 
 export const useAddTodoMutation = () => {
   const queryClient = useQueryClient();
 
   return useMutation({
     mutationFn: addTodo,
-    onSuccess: () =>
-      queryClient.invalidateQueries({ queryKey: [QUERY_KEY.TODO_LIST] }),
-    onError: (error) => {
+    onMutate: (newTodo: Omit<Todo, 'id'>) => {
+      queryClient.cancelQueries({ queryKey: [QUERY_KEY.TODO_LIST] });
+      const previousTodoList = queryClient.getQueriesData<Todo[]>({
+        queryKey: [QUERY_KEY.TODO_LIST],
+      });
+
+      queryClient.setQueryData([QUERY_KEY.TODO_LIST], (old: Todo[] = []) => {
+        return [...old, newTodo];
+      });
+
+      return { previousTodoList };
+    },
+    onError: (error, variables, context) => {
+      if (context && context.previousTodoList) {
+        queryClient.setQueryData(
+          [QUERY_KEY.TODO_LIST],
+          context.previousTodoList,
+        );
+      }
       toast.error(error.message);
     },
+    onSettled: () =>
+      queryClient.invalidateQueries({ queryKey: [QUERY_KEY.TODO_LIST] }),
   });
 };
