943465127ca711a55b7d19d583eed15b6bf4acea
parkminjo <cruiser367@naver.com>
2025-06-11 14:44:48 +0900

    design: 상하 padding값 수정

--- src/shared/layout/header.tsx
+++ src/shared/layout/header.tsx
@@ -3,7 +3,7 @@ import { PATH } from '@/shared/constants/path';
 
 const Header = () => {
   return (
-    <header className='px-4 py-2'>
+    <header className='px-4 py-3'>
       <Link href={PATH.HOME} className='font-semibold'>
         DODO
       </Link>
