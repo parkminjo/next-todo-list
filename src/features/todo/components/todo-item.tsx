5084216147ce66612eea14bd8f526d42ec85ccc3
parkminjo <cruiser367@naver.com>
2025-06-12 14:36:32 +0900

    refactor: 태그 구조 변경

--- src/features/todo/components/todo-item.tsx
+++ src/features/todo/components/todo-item.tsx
@@ -47,7 +47,7 @@ const TodoItem = ({ todo }: Props) => {
     }
 
     setIsEditMode(false);
-    updateTodoMutate({ todoId: todo.id, content });
+    updateTodoMutate({ todoId: todo.id, isDone: todo.isDone, content });
   };
 
   const handleChangeInput = (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -55,16 +55,13 @@ const TodoItem = ({ todo }: Props) => {
   };
 
   return (
-    <li>
-      <form
-        onSubmit={handleClickEditComplete}
-        className='flex h-[40px] items-center justify-between gap-6 py-2'
-      >
-        <div className='flex w-full items-center gap-3'>
-          <Checkbox
-            checked={todo.isDone ?? false}
-            onCheckedChange={handleClickCheckbox}
-          />
+    <li className='flex h-[40px] items-center justify-between gap-6 py-2'>
+      <div className='flex w-full items-center gap-2'>
+        <Checkbox checked={todo.isDone} onCheckedChange={handleClickCheckbox} />
+        <form
+          onSubmit={handleClickEditComplete}
+          className='flex w-full items-center justify-between gap-3'
+        >
           <input
             type='text'
             aria-label='할 일 내용'
@@ -73,19 +70,19 @@ const TodoItem = ({ todo }: Props) => {
             disabled={!isEditMode}
             className={`w-full bg-transparent outline-none ${isEditMode && 'border-b-2 border-gray-400'}`}
           />
-        </div>
 
-        {isEditMode ? (
-          <Button size='sm' aria-label='수정 완료'>
-            수정 완료
-          </Button>
-        ) : (
-          <TodoItemActionButtons
-            onClickDelete={handleClickDelete}
-            onClickEdit={handleClickEdit}
-          />
-        )}
-      </form>
+          {isEditMode ? (
+            <Button type='submit' size='sm' aria-label='수정 완료'>
+              수정 완료
+            </Button>
+          ) : (
+            <TodoItemActionButtons
+              onClickDelete={handleClickDelete}
+              onClickEdit={handleClickEdit}
+            />
+          )}
+        </form>
+      </div>
     </li>
   );
 };
