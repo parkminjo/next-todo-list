7b721ef4df96f210457eec4df8973d96e993db01
parkminjo <cruiser367@naver.com>
2025-06-19 19:31:23 +0900

    refactor: use-todo-input 훅으로 비즈니스 로직 분리

--- src/features/todo/components/todo-input.tsx
+++ src/features/todo/components/todo-input.tsx
@@ -1,52 +1,22 @@
 'use client';
 
-import { useState } from 'react';
-import { useAddTodoMutation } from '../hooks/use-add-todo-mutation';
-import { toast } from 'react-toastify';
-import { INFO_MESSAGE } from '@/shared/constants/info-message';
+import { useTodoInput } from '../hooks/use-todo-input';
 import type { SelectedDate } from '@/features/todo/types/todo.type';
-import { TOAST_ID } from '@/shared/constants/toast-id';
 
 interface Props {
   selectedDate: SelectedDate;
 }
 
 const TodoInput = ({ selectedDate }: Props) => {
-  const [content, setContent] = useState('');
-  const { mutate: addTodoMutate } = useAddTodoMutation();
-
-  const handleChangeInput = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setContent(event.target.value);
-  };
-
-  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-
-    if (content.trim() === '') {
-      toast.info(INFO_MESSAGE.TODO.EMPTY_INPUT, {
-        toastId: TOAST_ID.EMPTY_INPUT,
-      });
-      return;
-    }
-
-    if (selectedDate) {
-      addTodoMutate({
-        content,
-        date: selectedDate,
-        isDone: false,
-      });
-
-      setContent('');
-    }
-  };
+  const { content, onChange, onAddTodo } = useTodoInput(selectedDate);
 
   return (
-    <form onSubmit={handleSubmit}>
+    <form onSubmit={onAddTodo}>
       <input
         type='text'
         placeholder='할 일을 추가해주세요'
         value={content}
-        onChange={handleChangeInput}
+        onChange={onChange}
         className='w-full rounded-lg bg-gray-100 px-4 py-2 outline-gray-500'
       />
     </form>
---
+++ src/features/todo/hooks/use-todo-input.ts
@@ -0,0 +1,42 @@
+import { useState } from 'react';
+import { toast } from 'react-toastify';
+import { TOAST_ID } from '@/shared/constants/toast-id';
+import { INFO_MESSAGE } from '@/shared/constants/info-message';
+import { useAddTodoMutation } from '@/features/todo/hooks/use-add-todo-mutation';
+import type { SelectedDate } from '@/features/todo/types/todo.type';
+
+export const useTodoInput = (selectedDate: SelectedDate) => {
+  const [content, setContent] = useState('');
+  const { mutate: addTodoMutate } = useAddTodoMutation();
+
+  const handleChangeInput = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setContent(event.target.value);
+  };
+
+  const addTodo = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    if (content.trim() === '') {
+      toast.info(INFO_MESSAGE.TODO.EMPTY_INPUT, {
+        toastId: TOAST_ID.EMPTY_INPUT,
+      });
+      return;
+    }
+
+    if (selectedDate) {
+      addTodoMutate({
+        content,
+        date: selectedDate,
+        isDone: false,
+      });
+
+      setContent('');
+    }
+  };
+
+  return {
+    content,
+    onChange: handleChangeInput,
+    onAddTodo: addTodo,
+  };
+};
